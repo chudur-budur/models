23a524ada2aa0ae2afc8958041d5a55946cb0908
Khaled <khaled@nod-labs.com>
2021-03-23 07:35:04 +0000

    commit from gcp

--- train.sh
+++ train.sh
@@ -2,7 +2,7 @@ PROJ_HOME="$HOME/models"
 MODEL_DIR="$HOME/experiments/models/retinanet"
 PYTHONPATH=$PROJ_HOME CUDA_VISIBLE_DEVICES=1 python3 $PROJ_HOME/official/vision/detection/main.py \
     --strategy_type=one_device \
-    --num_gpus=2 \
+    --num_gpus=1 \
     --model_dir="${MODEL_DIR?}" \
     --mode=train \
     --config_file="$PROJ_HOME/configs/retinanet.yaml"
