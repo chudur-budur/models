f6caf52a80ae883362fc992857db0ee51baf7764
khaled <khaled@nod-labs.com>
2021-03-24 05:33:16 -0400

    yo

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 PROJ_HOME="$HOME/models"
 MODEL_DIR="$HOME/experiments/models/retinanet"
 CUDA_VISIBLE_DEVICES=1 PYTHONPATH=$PROJ_HOME python3 $PROJ_HOME/official/vision/detection/main.py \
-    --strategy_type=one_device \
-    --num_gpus=1 \
+    --strategy_type=mirrored \
+    --num_gpus=2 \
     --model_dir="${MODEL_DIR?}" \
     --mode=train \
     --config_file="$PROJ_HOME/configs/retinanet.yaml"
