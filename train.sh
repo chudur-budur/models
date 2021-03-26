7590504c11163869cc9b81dd7815e6c994009f20
khaled <khaled@nod-labs.com>
2021-03-26 19:22:02 -0400

    testing with spinenet190 with coco2017

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 PROJ_HOME="$HOME/models"
-MODEL_DIR="$HOME/experiments/models/retinanet"
+MODEL_DIR="$HOME/experiments/models/spinenet-coco2017"
 CUDA_VISIBLE_DEVICES=1 PYTHONPATH=$PROJ_HOME python3 $PROJ_HOME/official/vision/detection/main.py \
     --strategy_type=one_device \
     --num_gpus=1 \
     --model_dir="${MODEL_DIR?}" \
     --mode=train \
-    --config_file="$PROJ_HOME/configs/retinanet.yaml"
+    --config_file="$PROJ_HOME/configs/spinenet.yaml"
 
