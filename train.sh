09e8c7f9f00697c5cab2f844336a444dcc46cfac
Khaled <khaled@nod-labs.com>
2021-03-29 07:50:30 +0000

    new hyperparams

--- train.sh
+++ train.sh
@@ -1,10 +1,11 @@
 PROJ_HOME="$HOME/models"
-MODEL_DIR="$HOME/experiments/models/retinanet-sp96-bs16-lr0.06"
-CUDA_VISIBLE_DEVICES=1 PYTHONPATH=$PROJ_HOME \
+MODEL_DIR="$HOME/experiments/models/retinanet-sp96-bs4-lr0.06"
+CUDA_VISIBLE_DEVICES=1 TF_FORCE_GPU_ALLOW_GROWTH=true PYTHONPATH=$PROJ_HOME \
     python $PROJ_HOME/official/vision/detection/main.py \
-    --strategy_type=one_device \
-    --num_gpus=1 \
+    --strategy_type=mirrored \
+    --num_gpus=2 \
+    --all_reduce_alg=nccl \
     --model_dir="${MODEL_DIR?}" \
     --mode=train \
-    --config_file="$PROJ_HOME/configs/retinanet-sp96-bs16-lr0.06.yaml"
+    --config_file="$PROJ_HOME/configs/retinanet-sp96-bs4-lr0.06.yaml"
 
