ce7bdc979ad48b74ff529654410ba9a808b97af7
khaled <khaled@nod-labs.com>
2021-03-29 02:28:10 -0400

    adding more networks

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 PROJ_HOME="$HOME/models"
-MODEL_DIR="$HOME/experiments/models/retinanet-spine49-coco2017"
+MODEL_DIR="$HOME/experiments/models/retinanet-sp96-bs16-lr0.06"
 CUDA_VISIBLE_DEVICES=1 PYTHONPATH=$PROJ_HOME \
     python $PROJ_HOME/official/vision/detection/main.py \
     --strategy_type=one_device \
     --num_gpus=1 \
     --model_dir="${MODEL_DIR?}" \
     --mode=train \
-    --config_file="$PROJ_HOME/configs/retinanet-spine49.yaml"
+    --config_file="$PROJ_HOME/configs/retinanet-sp96-bs16-lr0.06.yaml"
 
