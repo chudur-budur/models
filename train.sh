9a0c7f998fc6ecb05b1725d614b5d4bef0bc507b
Khaled <khaled@nod-labs.com>
2021-03-29 08:05:42 +0000

    trial and error

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 PROJ_HOME="$HOME/models"
-MODEL_DIR="$HOME/experiments/models/retinanet-sp96-bs6-lr0.04"
+MODEL_DIR="$HOME/experiments/models/retinanet-sp96-bs4-lr0.02"
 CUDA_VISIBLE_DEVICES=1 TF_FORCE_GPU_ALLOW_GROWTH=true PYTHONPATH=$PROJ_HOME \
     python $PROJ_HOME/official/vision/detection/main.py \
     --strategy_type=mirrored \
@@ -7,5 +7,5 @@ CUDA_VISIBLE_DEVICES=1 TF_FORCE_GPU_ALLOW_GROWTH=true PYTHONPATH=$PROJ_HOME \
     --all_reduce_alg=nccl \
     --model_dir="${MODEL_DIR?}" \
     --mode=train \
-    --config_file="$PROJ_HOME/configs/retinanet-sp96-bs6-lr0.04.yaml"
+    --config_file="$PROJ_HOME/configs/retinanet-sp96-bs4-lr0.02.yaml"
 
