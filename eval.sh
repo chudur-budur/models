1235d2da003aaefcdf63382818cd69969aa8001b
khaled <khaled@nod-labs.com>
2021-03-29 02:28:10 -0400

    adding more networks

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
 PROJ_HOME="$HOME/models"
-MODEL_DIR="$HOME/experiments/models/retinanet-spine49-coco2017"
+MODEL_DIR="$HOME/experiments/models/retinanet-sp96-bs16-lr0.06"
 CUDA_VISIBLE_DEVICES=1 TF_FORCE_GPU_ALLOW_GROWTH=true PYTHONPATH=$PROJ_HOME \
     python3 $PROJ_HOME/official/vision/detection/main.py \
     --strategy_type=one_device \
@@ -7,4 +7,4 @@ CUDA_VISIBLE_DEVICES=1 TF_FORCE_GPU_ALLOW_GROWTH=true PYTHONPATH=$PROJ_HOME \
     --model_dir="${MODEL_DIR?}" \
     --mode=eval \
     --model=retinanet \
-    --config_file="$PROJ_HOME/configs/retinanet-spine49.yaml"
+    --config_file="$PROJ_HOME/configs/retinanet-sp96-bs16-lr0.06.yaml"
